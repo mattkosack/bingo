7fc0162adaaf6836b035d3fb4cc05bbf6e8b5924
Matthew Kosack <kosackm@moravian.edu>
2022-02-08 17:20:04 -0500

    more topics

--- bingo.js
+++ bingo.js
@@ -7,7 +7,7 @@ let phrases = ["Green to Green", "Jeep Talk", "The Hands",
                 "Condes- cension", "You Type Slow", "Abstraction", 
                 "Send Me An Email", "Long Plickers","Uncle Bob",
                 "Hiking","Over Time", "Says he's a Geek", "Github Workflow",
-                "REST API","Profession- alism","i","k","j","l"];
+                "REST API","Profession- alism","Beer","Tea"];
 
 let copy_phrases;
 
