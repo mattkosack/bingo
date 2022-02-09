e73d0a6fb1851633fdbe3c0741458fa8422e9915
Matthew Kosack <kosackm@moravian.edu>
2022-02-09 16:35:18 -0500

    more topics

--- bingo.js
+++ bingo.js
@@ -1,15 +1,16 @@
 let usedNums = new Array(11);
 let usedPhrases = new Array(11);
 
-let phrases = ["Green to Green", "Jeep Talk", "The Hands", 
-                "Learning Cycle", "Unnoticed Typo", "Shouting", 
-                "Comma", "Poor Spelling", "Talk About Weekend", "Public Shaming",
-                "Condes- cension", "You Type Slow", "Abstraction", 
-                "Send Me An Email", "Long Plickers","Uncle Bob",
-                "Hiking","Over Time", "Says he's a Geek", "Github Workflow",
-                "REST API", "Profession- alism","Beer","Tea", "HTTP is Just Text",
-                "gitkeeper"
-            ];
+let phrases = [
+        "Green to Green", "Jeep Talk", "The Hands", 
+        "Learning Cycle", "Unnoticed Typo", "Shouting", 
+        "Comma", "Poor Spelling", "Talk About Weekend", "Public Shaming",
+        "Condes- cension", "You Type Slow", "Abstraction", 
+        "Send Me An Email", "Long Plickers","Uncle Bob",
+        "Hiking", "Over Time", "Says he's a Geek", "Github Workflow",
+        "REST API", "Profession- alism","Beer","Tea", "HTTP is Just Text",
+        "gitkeeper", "'word' instead of swear", "dotenv/.env", "thing <b>is</b> thing" 
+    ];
 
 let copy_phrases;
 
