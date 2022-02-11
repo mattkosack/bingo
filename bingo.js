44a5a251a598388400f305123f7ce932a0df7949
Jeffrey Bush <jeff@coderforlife.com>
2022-02-11 14:51:20 -0500

    Support seeding the RND with the location hash

--- bingo.js
+++ bingo.js
@@ -13,8 +13,54 @@ let phrases = [
 
 let darkMode = window.localStorage.getItem("darkMode") === "true";
 
+
+/**
+ * Get a random number generator, optionally based on a predetermined seed. The
+ * seed can be a string (or really any object) in which case it is hashed or an
+ * integer which is used directly.
+ * @param {*} seed string or number to use to seed the generator
+ * @returns a function that takes no arguments and returns a new random number
+ * when called
+ */
+function random_generator(seed) {
+    if (!Number.isInteger(seed)) {
+        // Hash the string using MurmurHash3
+        // Source: https://stackoverflow.com/a/19301306/582298
+        seed = seed.toString();
+        let h = 1779033703 ^ seed.length;
+        for(let i = 0; i < seed.length; i++) {
+            h = Math.imul(h ^ seed.charCodeAt(i), 3432918353);
+            h = h << 13 | h >>> 19;
+        }
+        h = Math.imul(h ^ (h >>> 16), 2246822507);
+        h = Math.imul(h ^ (h >>> 13), 3266489909);
+        seed = (h ^= h >>> 16) >>> 0;
+    }
+
+    // Creates a mulberry32 random number generator.
+    // Source: https://stackoverflow.com/a/19301306/582298
+    let rand = function () {
+        let t = seed += 0x6D2B79F5;
+        t = Math.imul(t ^ t >>> 15, t | 1);
+        t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+        return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    };
+
+    // Mix up the initial state
+    for (let i = 0; i < 15; i++) { rand(); }
+    
+    // return the random number generating function
+    return rand;
+}
+
+function reseed() {
+    window.location.hash = Math.random().toString();
+}
+
 function newCard() {
-    let shuffled = phrases.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
+    if (!window.location.hash) { reseed(); }
+    let rand = random_generator(window.location.hash);
+    let shuffled = phrases.map(value => ({ value, sort: rand() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
     for (let i = 0; i < 24; i++) {
         let elem = document.getElementById("square"+i);
         elem.innerHTML = shuffled[i];
