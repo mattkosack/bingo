cbf0b8236330efd63b073b6a5745566dcffbff32
Jeffrey Bush <jeff@coderforlife.com>
2022-02-11 12:27:40 -0500

    Fix the absolute disaster that was colors, clicking, and dark mode.

--- bingo.js
+++ bingo.js
@@ -15,38 +15,11 @@ let copy_phrases;
 let usedNums = new Array(phrases.length);
 let usedPhrases = new Array(phrases.length);
 
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires;
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return false;
-}
-
-let darkMode = false;
-if (getCookie("darkMode") === "true") {
-    darkMode = true;
-}
+let darkMode = window.localStorage.getItem("darkMode") === "true";
 
 function newCard() {
     copy_phrases = phrases.map((x) => x); 
-    for(let i = 0; i < 24; i++) { setSquare(i); } 
-    initEvents();
+    for (let i = 0; i < 24; i++) { setSquare(i); } 
 }
 
 function setSquare(thisSquare) {
@@ -55,11 +28,11 @@ function setSquare(thisSquare) {
 	
 	do {
         newPhrase = getNewPhrase()
-	}
-	while (usedNums[newNum]);
+	} while (usedNums[newNum]);
 	
     usedPhrases[newNum] = true;
 	document.getElementById(currSquare).innerHTML = newPhrase;
+    document.getElementById(currSquare).classList.remove("clicked");
 }
 
 function getNewPhrase() {
@@ -70,66 +43,34 @@ function getNewPhrase() {
 }
 
 function anotherCard() {
-	for(let i = 1; i < usedNums.length; i++) { usedNums[i] = false; }
+	for (let i = 1; i < usedNums.length; i++) { usedNums[i] = false; }
 	newCard();
 }
 
 function onClick(e) {
-    if (e.target.style.backgroundColor === "cyan") {
-        e.target.style.backgroundColor = "white";
-    } else if (e.target.style.backgroundColor === "white") {
-        e.target.style.backgroundColor = "cyan";
-    } else if (e.target.style.backgroundColor === "rgb(153, 170, 181)") {
-        e.target.style.backgroundColor = "rgb(114, 137, 218)";
-    } else if (e.target.style.backgroundColor === "rgb(114, 137, 218)") {
-        e.target.style.backgroundColor = "rgb(153, 170, 181)";
-    }
+    e.target.classList.toggle("clicked");
 }
 
 function initEvents() {
-    let color = "rgb(153, 170, 181)";
-    if (!darkMode) {
-        color = "white";
-    }
-    for (let i = 0; i < 24; i++) { 
+    for (let i = 0; i < 24; i++) {
         document.getElementById("square"+i).addEventListener("click", onClick);
-        document.getElementById("square"+i).style.backgroundColor = color; 
     }
     setColorModeColors(darkMode);
 }
 
-function switchSquareColor(square) {
-    if (square.style.backgroundColor === "white") {
-        square.style.backgroundColor = "rgb(153, 170, 181)";
-    } else if (square.style.backgroundColor === "rgb(153, 170, 181)") {
-        square.style.backgroundColor = "white";
-    } else if (square.style.backgroundColor === "cyan") {
-        square.style.backgroundColor = "rgb(114, 137, 218)";
-    } else if (square.style.backgroundColor === "rgb(114, 137, 218)") {
-        square.style.backgroundColor = "cyan";
-    }
-}
-
 function setColorModeColors(mode) {
-    let button = document.querySelector("#colorMode");
-    if (mode) {
-        darkMode = true;
-        document.body.style.backgroundColor= "#23272a";
-        button.innerText = "Light Mode";
-        document.querySelector("#squarefree").style.background = "rgb(114, 137, 218)";
-    } else {
-        darkMode = false;
-        document.body.style.backgroundColor= "#1abc9c";
-        button.innerText = "Dark Mode";
-        document.querySelector("#squarefree").style.background = "cyan";
+    document.getElementById("colorMode").innerText = mode ? "Light Mode" : "Dark Mode";
+    darkMode = mode;
+    if (darkMode) {
+        document.body.classList.add('darkMode');
+    }
+    else {
+        document.body.classList.remove('darkMode');
     }
 }
 
 function switchColorMode() {
     /* Swap text and color to change color mode */
     setColorModeColors(!darkMode);
-    for (let i = 0; i < 24; i++) {
-        switchSquareColor(document.querySelector("#square" + i));
-    }
-    setCookie("darkMode", darkMode.toString(), 31);
+    window.localStorage.setItem('darkMode', darkMode.toString());
 }
